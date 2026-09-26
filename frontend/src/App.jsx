a947b4991fd0c0af434b94df8de0ea6e0cca6227
DailyDevNguyen <dailydev.nguyen@gmail.com>
2026-09-26 12:13:51 +0300

    Fix indentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,12 @@ import {Routes, Route} from "react-router-dom"
 function App() {
   return (
     <>
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   )
 }
