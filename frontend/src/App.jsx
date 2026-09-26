01c52930c2a3466500ac677558c55aeb31cf0d27
DailyDevNguyen <dailydev.nguyen@gmail.com>
2026-09-26 12:11:37 +0300

    Add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Home from "./pages/Home.jsx"
 import Favorites from "./pages/Favorites.jsx"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Favorites />
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
     </>
   )
 }
