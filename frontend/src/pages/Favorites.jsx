99eb92aca1a9dc9a37619b6091af100cabd62c25
DailyDevNguyen <dailydev.nguyen@gmail.com>
2026-09-26 14:47:57 +0300

    Fix quotes

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,7 +1,7 @@
 function Favorites() {
     return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
+        <h2>No Favorite Animes Yet</h2>
+        <p>Start adding animes to your favorites and they will appear here</p>
     </div>
 }
 
