50b7a2c6746694ac4ae72f5cf36aace33136e8c4
DailyDevNguyen <dailydev.nguyen@gmail.com>
2026-09-26 12:44:42 +0300

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home.jsx"
 import Favorites from "./pages/Favorites.jsx"
+import Navbar from "./components/Navbar.jsx"
 import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
+      <Navbar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
